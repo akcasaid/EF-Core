1eefc8ef6226fef0ed0abd5cc4298660938e56fb
Said Akça <56161195+akcasaid@users.noreply.github.com>
2024-03-16 23:07:02 +0300

    Add files via upload

---
+++ UdemyEFCore.DatabaseFirst/DAL/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyEFCore.DatabaseFirst.DAL
+{
+    public class AppDbContext:DbContext // Db context sınıfından miras aldım bu bize entitiy framework paketinden geliyor
+    {
+        public DbSet<Product> Products { get; set; }
+
+
+        public AppDbContext()
+        {
+
+        }
+
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
